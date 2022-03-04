88d19a0e502423ea39c8b12597456ecc335a0cf0
banannakn <54701902+banannakn@users.noreply.github.com>
2022-03-04 15:25:40 +0100

    Update sheet_image_loader.py
    Added possibility to read multiple images from one cell

--- openpyxl_image_loader/sheet_image_loader.py
+++ openpyxl_image_loader/sheet_image_loader.py
@@ -4,7 +4,7 @@ Contains a SheetImageLoader class that allow you to loadimages from a sheet
 
 import io
 import string
-
+import itertools
 from PIL import Image
 
 
@@ -13,14 +13,17 @@ class SheetImageLoader:
 
     def __init__(self, sheet):
         """Loads all sheet images"""
-        
         self._images = {}
-        
+        # Holds an array of A-ZZ
+        col_holder = list(itertools.chain(string.ascii_uppercase, (''.join(pair) for pair in itertools.product(string.ascii_uppercase, repeat=2))))
         sheet_images = sheet._images
         for image in sheet_images:
             row = image.anchor._from.row + 1
-            col = string.ascii_uppercase[image.anchor._from.col]
-            self._images[f'{col}{row}'] = image._data
+            col = col_holder[image.anchor._from.col] # Convert the col to correct Excel Col.
+            if self._images.get(f'{col}{row}'): # Append multiple images of one cell
+                self._images[f'{col}{row}'].append(image._data)
+            else:
+                self._images[f'{col}{row}'] = [image._data]
 
     def image_in(self, cell):
         """Checks if there's an image in specified cell"""
@@ -31,5 +34,11 @@ class SheetImageLoader:
         if cell not in self._images:
             raise ValueError("Cell {} doesn't contain an image".format(cell))
         else:
-            image = io.BytesIO(self._images[cell]())
-            return Image.open(image)
+            for ele in self._images[cell]: # Show all images of one cell 
+                image = io.BytesIO(ele())
+                display(Image.open(image))
+                image.close()
+
+    def close(self):
+        """Delete image buffer"""
+        self._images.clear()

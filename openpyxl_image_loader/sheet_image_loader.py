21e81b266e082b4d8120d1f561bdb995608e84cf
Joe Marchionna <joemarchionna@gmail.com>
2022-04-02 10:33:23 -0400

    Fixed issue of image not assigned / related to a cell, where there is no "from" xml tag.

--- openpyxl_image_loader/sheet_image_loader.py
+++ openpyxl_image_loader/sheet_image_loader.py
@@ -13,14 +13,15 @@ class SheetImageLoader:
 
     def __init__(self, sheet):
         """Loads all sheet images"""
-        
+
         self._images = {}
-        
+
         sheet_images = sheet._images
         for image in sheet_images:
-            row = image.anchor._from.row + 1
-            col = string.ascii_uppercase[image.anchor._from.col]
-            self._images[f'{col}{row}'] = image._data
+            if hasattr(image.anchor, "_from"):
+                row = image.anchor._from.row + 1
+                col = string.ascii_uppercase[image.anchor._from.col]
+                self._images[f"{col}{row}"] = image._data
 
     def image_in(self, cell):
         """Checks if there's an image in specified cell"""
